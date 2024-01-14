4d370e81accf04fdde4c7529bc28a6ccd2527da3
June-64 <473475792@qq.com>
2024-01-14 23:03:12 +0800

    1

--- uni/pages/index/home.vue
+++ uni/pages/index/home.vue
@@ -1,10 +1,10 @@
 <template>
   <cl-page status-bar-background="transparent">
-    <view>
-      <view>
+    <view class="main-container">
+      <view class="teacher ">
         我是老师
       </view>
-      <view>
+      <view class="student">
         我是学生
       </view>
     </view>
@@ -24,5 +24,43 @@ const app = useApp();
 </script>
 
 <style lang="scss" scoped>
+.main-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  color: white;
+  position: fixed;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.main-container .teacher, .main-container .student {
+  line-height: 200rpx;
+  text-align: center;
+  border-radius: 20rpx;
+  width: 350rpx;
+  height: 200rpx;
+  margin-bottom: 40rpx;
+  transition: transform 0.3s ease; /* 定义过渡效果 */
+  cursor: pointer; /* 鼠标悬停时显示手势 */
+  font-size: 1.2em;
+}
+
+.main-container .teacher {
+  background-color: #fd962e;
+}
+
+.main-container .student {
+  background-color: #6363fd;
+  margin-bottom: 0; /* 学生按钮没有底部边距 */
+}
+
+/* 伪类添加按下时的缩放效果 */
+.main-container .teacher:active, .main-container .student:active {
+  transform: scale(0.9); /* 缩小到90%的原尺寸 */
+}
 
 </style>
